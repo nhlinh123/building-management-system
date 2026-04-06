1b7c55bb7aa1b546113f97eb09a07da34598fd85
linhnh <nhlinh123@gmail.com>
2026-04-07 05:58:47 +0700

    add seed prisma user

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,8 +7,10 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: 'node ./prisma/seed.ts',
   },
   datasource: {
     url: process.env["DATABASE_URL"],
   },
+  
 });
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,22 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+});
 
 async function main() {
+  const hashedPassword = await bcrypt.hash('admin123', 10);
   await prisma.user.createMany({
     data: [
       {
         name: 'Admin',
         email: 'admin@bms.com',
+        password: hashedPassword,
         role: 'admin',
         isActive: true,
-      },
-      {
-        name: 'User 1',
-        email: 'user1@bms.com',
-        role: 'user',
-        isActive: true,
-      },
+      }
     ],
   });
 
